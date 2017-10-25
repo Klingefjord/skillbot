aa36c3227a94e6e861679170e6a0a02e39c300cf
Klingefjord <oliver.klingefjord@hyperisland.se>
2017-10-25 21:15:26 +0200

    Oauth from db in user

--- user.js
+++ user.js
@@ -1,23 +1,26 @@
 const request = require('request');
+const oauth = require('./db/oauth');
 
 /*
 *       Returns promise of slack JSON user details based on db user input
 */
-module.exports = function findUserProfile(user) { 
-    const access_token = process.env.TOKEN;
+module.exports = function findUserProfile(user_id, team_id) { 
+    
     const clientId = process.env.SLACK_CLIENT_ID;
     const clientSecret = process.env.SLACK_CLIENT_SECRET;
 
     return new Promise((resolve, reject) => {
-        request({
-            url: 'https://slack.com/api/users.profile.get', //URL to hit
-            qs: {token: access_token, user: user.user_id, client_id: clientId, client_secret: clientSecret}, //Query string data
-            method: 'GET', //Specify the method
-        }, function (error, response, body) {
-            if (error) reject(error);
-            else {
-                resolve(JSON.parse(body).profile); 
-            }
+        oauth.get(team_id).then((token) => {
+            request({
+                url: 'https://slack.com/api/users.profile.get',
+                qs: {token: token, user: user_id, client_id: clientId, client_secret: clientSecret},
+                method: 'GET',
+            }, function (error, response, body) {
+                if (error) reject(error);
+                else {
+                    resolve(JSON.parse(body).profile); 
+                }
+            });  
         });
     });
 }
